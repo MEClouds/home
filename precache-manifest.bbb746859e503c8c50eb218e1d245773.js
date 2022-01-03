22b63975338a8b8ec0ae1f8204354c03193c0cc7
Ayman Mustafa <mswain6@saddleback.edu>
2022-01-04 01:25:45 +0200

    Updates

---
+++ precache-manifest.bbb746859e503c8c50eb218e1d245773.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f4bdb21fb7e551b6323b4b97fc0ba62",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "29ec866e82981c5c48fb",
+    "url": "/home/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "2a7bc1991df440a50bca",
+    "url": "/home/static/css/main.594b7a92.chunk.css"
+  },
+  {
+    "revision": "29ec866e82981c5c48fb",
+    "url": "/home/static/js/2.90cf4ce6.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/home/static/js/2.90cf4ce6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a7bc1991df440a50bca",
+    "url": "/home/static/js/main.f80de26e.chunk.js"
+  },
+  {
+    "revision": "bac681e6707b590225d4",
+    "url": "/home/static/js/runtime-main.caf7a1cd.js"
+  }
+]);
